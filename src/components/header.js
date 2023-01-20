7733c6c5e98461568f905d3ebddb756636e79281
Savvas Stephanides <savvascyp@hotmail.com>
2023-01-20 11:07:15 +0000

    Google Form as contact dev

--- src/components/header.js
+++ src/components/header.js
@@ -15,7 +15,7 @@ function Header(){
     return (
         <header style={style.header}>
             <img src={`${process.env.PUBLIC_URL}/logo.png`} alt="" style={style.logo}/>
-            <a href="mailto:savvascyp@hotmail.com" style={{
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSfQejpGGov9zA0wWYfVy7HGMeOA9lv0eQYYBupOdmymqtmPsw/viewform?usp=sf_link" style={{
                 "display": "block",
                 "position": "absolute",
                 top: "30px",
